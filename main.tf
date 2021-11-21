b0980d5ae15ce5539fd8ef53d1798996e821d871
unknown <gideonochiengokwach@gmail.com>
2021-11-21 19:11:38 +0300

    restructed terraform configuration, terratest

--- main.tf
+++ main.tf
@@ -1,11 +1,19 @@
+# terraform {
+#   required_providers {
+#     aws = {
+#       source = "hashicorp/aws"
+#     }
+#   }
+# }
 terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
+  backend "remote" {
+    organization = "Zaam-Technologies"
+
+    workspaces {
+      name = "terraformAssignment"
     }
   }
 }
-
 provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
